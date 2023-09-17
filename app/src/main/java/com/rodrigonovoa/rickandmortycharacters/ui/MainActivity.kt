dfee5de6b6be174a60edc29257f088db740404e9
rodrigonovoasalgado <rodrigonovoa97s@gmail.com>
2023-09-17 11:58:13 +0200

    project organization

--- app/src/main/java/com/rodrigonovoa/rickandmortycharacters/ui/MainActivity.kt
+++ app/src/main/java/com/rodrigonovoa/rickandmortycharacters/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rodrigonovoa.rickandmortycharacters
+package com.rodrigonovoa.rickandmortycharacters.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rodrigonovoa.rickandmortycharacters.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
